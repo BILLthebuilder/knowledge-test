135e982ffc21c5cc257de94989b0e2b13d69f490
Bill Kariri <billkariri@gmail.com>
2020-02-15 20:24:39 +0300

    warap the whole app in a router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Todologic from './components/TodoLogic';
 import Header from './components/Header';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 export default class App extends Component {
 
   render() {
     return (
       <React.Fragment>
-        <Header/>
-        <Todologic />
+        <Router>
+          <Header />
+          <Todologic />
+        </Router>
       </React.Fragment>
     )
   }
