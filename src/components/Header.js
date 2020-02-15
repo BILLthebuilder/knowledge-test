4d8d2cab502aca688ef5e379b6eb04a2967c1b7d
Bill Kariri <billkariri@gmail.com>
2020-02-15 09:40:40 +0300

    the header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            background: 'black',
+            color: 'white',
+            margin: 0,
+            padding:0
+        }}>
+            Todo App experiment
+        </div>
+    )
+}
